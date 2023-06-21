cba1db46082d09f8aca27ed6479a12dbae1acb2e
Owiti95 <hanningtonowiti@gmail.com>
2023-06-21 07:36:52 -0700

    putchar

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * print_alphabet - print all alphabet in lowercase
+ *
+ * Return:void
+ */
+
+void print_alphabet(void)
+{
+	char letter;
+
+	for (letter = 'a'; letter <= 'z'; letter++)
+		-putchar(letter);
+
+
+		_putchar('\n');
+}
