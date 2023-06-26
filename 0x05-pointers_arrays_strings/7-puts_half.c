62385299ddd5bbbf61584b8864f9637d778c483e
Owiti95 <hanningtonowiti@gmail.com>
2023-06-26 13:52:21 -0700

    Arrays are not pointers

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -1,10 +1,11 @@
 #include "main.h"
 /**
-* Puts_half - A function that prints half of a string
-* If odd len, n = (length_of_the_string - 1) / 2
-* @str: input
-* Return: half of input
-*/
+ * Puts_half - A function that prints half of a string
+ * If odd len, n = (length_of_the_string - 1) / 2
+ * @str: input
+ * Return: half of input
+ */
+
 void puts_half(char *str)
 {
 	int a, n, longi;
